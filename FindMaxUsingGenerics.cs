fd72a53436fb3013f31baf59d85f6115cc21ac87
Anisha0906 <anishadas880@gmail.com>
2022-08-10 20:39:31 +0530

    Add: Find Maximum Number Using Generic Concept

---
+++ FindMaxUsingGenerics.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxUsingGenerics
+{
+    public class MaxNumberCheck
+    {
+        
+        public static int MaximumIntegerNumber(int firstValue, int secondValue, int thirdValue)
+        
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+                return firstValue;
+            
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+                
+                return secondValue;
+            else
+                
+                return thirdValue;
+
+        }
+       
+        public static double MaximumFloatNumber(double firstValue, double secondValue, double thirdValue)
+        {
+            
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+               
+                return firstValue;
+           
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+                
+                return secondValue;
+            else
+                
+                return thirdValue;
+        }
+        public static string MaximumString(string firstString, string secondString, string thirdString)
+        {
+           
+            if (firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) > 0 ||
+                firstString.CompareTo(secondString) >= 0 && firstString.CompareTo(thirdString) > 0 ||
+                firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) >= 0)
+                return firstString;
+
+            if (secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) > 0 ||
+                secondString.CompareTo(firstString) >= 0 && secondString.CompareTo(thirdString) > 0 ||
+                secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) >= 0)
+                return secondString;
+
+            if (thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) > 0 ||
+                thirdString.CompareTo(firstString) >= 0 && thirdString.CompareTo(secondString) > 0 ||
+                thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) >= 0)
+                return thirdString;
+
+            throw new Exception("firstString, secondString and thirdString are same");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxUsingGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max Using Generics");
+            MaxNumberCheck maxNumberCheck=new MaxNumberCheck();
+            Console.ReadLine();
+        }
+    }
+}
